4ff7dfbc2d9560764804dd3e09d3eeafd039f007
vtesoho <vtesoho@163.com>
2019-07-21 23:28:53 +0800

    组合

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-let prototype = {
-  getName: function(){
-    return this.first + ' ' + this.last
-  },
-  say: function (){
-    console.log('hello')
+var num = 0,i=0,a=0,arr = new Array(5)
+function dg(){
+  num = Math.floor(Math.random() * (2 - 32)) + 32
+  if(arr[a] === num &&  a < arr.length){
+    a++
+    dg()
+  }
+  if(arr[i] === undefined && i < arr.length){
+    arr[i] = num
+    a=0
+    i++
+    dg()
   }
 }
 
-//基于原型创建 x
-let x = Object.create(prototype)
-x.first = "A"
-x.last = "B"
-console.log(x.getName())
-x.say()
 
-//基于原型创建 y
-let y = Object.create(prototype)
-y.first = "C"
-y.last = "D"
-console.log(y.getName())
-y.say()
+for (let index = 0; index < 10; index++) {
+  dg()
+  console.log(arr)
+
+  arr[0] = undefined
+  arr[1] = undefined
+  arr[2] = undefined
+  arr[3] = undefined
+  arr[4] = undefined
+  
+}

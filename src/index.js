b9e2e165311c77a11e91f19842f299c7e007cd9c
vtesoho <vtesoho@163.com>
2019-07-29 22:16:09 +0800

    mediator

--- src/index.js
+++ src/index.js
@@ -1,56 +1,49 @@
-//备忘的类
-class Memento{
-  constructor(content){
-    this.content = content
-  }
-  getContent(){
-    return this.content
-  }
-}
-//备忘列表
-class CareTaker{
+class A {
   constructor(){
-    this.list = []
+    this.number = 0
   }
-  add(memento){
-    this.list.push(memento)
-  }
-  get(index){
-    return this.list[index]
+  setNumber(num, m){
+    this.number = num
+    if (m) {
+      m.setB()
+    }
   }
 }
 
-//编辑器
-class Editor{
+class B {
   constructor(){
-    this.content = null
+    this.number = 0
   }
-  setContent(content) {
-    this.content = content
-  }
-  getContent(){
-    return this.content
-  }
-  saveContentToMemento(){
-    return new Memento(this.content)
-  }
-  getContentFromMemento(memento){
-    this.content = memento.getContent()
+  setNumber(num, m){
+    this.number = num
+    if (m) {
+      m.setA()
+    }
   }
 }
 
-let editor = new Editor()
-let careTaker = new CareTaker()
+class Mediator{
+  constructor(a,b) {
+    this.a = a
+    this.b = b
+  }
+  setB(){
+    let number = this.a.number
+    this.b.setNumber(number * 100)
+  }
+  setA(){
+    let number = this.a.number
+    this.a.setNumber(number / 100)
+  }
+}
 
-editor.setContent('111')
-editor.setContent('222')
-careTaker.add(editor.saveContentToMemento())
-editor.setContent('333')
-careTaker.add(editor.saveContentToMemento())
-editor.setContent('444')
+let a = new A()
+let b = new B()
+let m = new Mediator(a,b)
+a.setNumber(100, m)
+console.log(a.number,b.number)
+b.setNumber(100, m)
+console.log(a.number,b.number)
 
-console.log(editor.getContent())
-editor.getContentFromMemento(careTaker.get(1))
-console.log(editor.getContent())
-editor.getContentFromMemento(careTaker.get(0))
-console.log(editor.getContent())
+
+
